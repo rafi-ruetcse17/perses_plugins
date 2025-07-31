da77dc94b8ee2b072ce7bc5f820a3e9b6f3c18a9
rafi <rafialam@appscode.com>
2025-07-31 18:44:34 +0600

    Revert "Integrate clickhouse query plugin"
    This reverts commit 127e60817c4f79c77c7d206124e9b16ec6b281d5.
    
    Signed-off-by: rafi <rafialam@appscode.com>

--- clickhouse/src/queries/click-house-query/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./constants";
-export * from "./get-time-series-data";
-export * from "./ClickHouseQuery";
-export * from "./ClickHouseQueryEditor";
-export * from "./click-house-query-types";
