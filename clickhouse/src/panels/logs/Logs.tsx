437ad22b7a0d44b8ecbed93c77ed998f5b1f7023
rafi <rafialam@appscode.com>
2025-08-04 18:19:22 +0600

    Integrate panel plugin for logs
    Signed-off-by: rafi <rafialam@appscode.com>

---
+++ clickhouse/src/panels/logs/Logs.tsx
@@ -0,0 +1,11 @@
+import { LogsComponent } from "./LogsComponent";
+import { LogsOptions, LogsProps } from "./logs-types";
+import { PanelPlugin } from "@perses-dev/plugin-system";
+import { LogsSettingsEditor } from "./LogsSettingsEditor";
+
+export const Logs: PanelPlugin<LogsOptions, LogsProps> = {
+  PanelComponent: LogsComponent,
+  panelOptionsEditorComponents: [{ label: 'Settings', content: LogsSettingsEditor }],
+  supportedQueryTypes: ['TimeSeriesQuery'],
+  createInitialOptions: () => ({}),
+};
