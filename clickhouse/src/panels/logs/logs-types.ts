c752cd2363b07f731ded9a2b37f254732cee4a5d
rafi <rafialam@appscode.com>
2025-08-04 18:19:22 +0600

    Integrate panel plugin for logs
    Signed-off-by: rafi <rafialam@appscode.com>

---
+++ clickhouse/src/panels/logs/logs-types.ts
@@ -0,0 +1,19 @@
+import { TimeSeriesData, ThresholdOptions } from "@perses-dev/core";
+import { PanelProps, LegendSpecOptions } from '@perses-dev/plugin-system';
+
+export type QueryData = TimeSeriesData; // Type of data returned by a query plugin and supported by this plugin
+
+export type LogsProps = PanelProps<LogsOptions, QueryData>;
+
+export interface QuerySettingsOptions {
+  queryIndex: number;
+  colorMode: 'fixed' | 'fixed-single';
+  colorValue: string;
+}
+
+export interface LogsOptions {
+  legend?: LegendSpecOptions;
+  thresholds?: ThresholdOptions;
+  querySettings?: QuerySettingsOptions;
+}
+
