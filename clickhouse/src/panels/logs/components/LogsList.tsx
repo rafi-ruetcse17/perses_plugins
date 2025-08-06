5bca01c2dc39c67c471d6251195aa301c976657c
rafi <rafialam@appscode.com>
2025-08-06 19:05:19 +0600

    Integrate UI to show log panel
    Signed-off-by: rafi <rafialam@appscode.com>

---
+++ clickhouse/src/panels/logs/components/LogsList.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { LogEntry } from '../../../model/click-house-data-types';
+import { LogsOptions } from '../logs-types';
+import { EmptyLogsState } from './EmptyLogsState';
+import { useExpandedRows } from './hooks/useExpandedRows';
+
+interface LogsListProps {
+  logs: LogEntry[];
+  spec: LogsOptions;
+}
+
+const LogsList: React.FC<LogsListProps> = ({ logs, spec }) => {
+  const { expandedRows, toggleExpand } = useExpandedRows();
+
+  if (!logs.length) {
+    return <EmptyLogsState />;
+  }
+
+  // return <VirtualizedLogsList logs={logs} spec={spec} expandedRows={expandedRows} onToggleExpand={toggleExpand} />;
+};
+
+export default LogsList;
